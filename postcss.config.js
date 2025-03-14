cf0b1ee62f43930a10bac14e28fe9c1a11d75bc4
jotaveeo <joaovitorgoku10@gmail.com>
2025-03-14 15:45:23 -0300

    formata arquivos de configuração e reativa o link para o CSS principal

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// filepath: c:\Users\joaov\Documents\Projetos Programação\Codece\projeto\postcss.config.js
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
